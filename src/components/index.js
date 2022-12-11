4ee3b0565988c2448626bd177e4348fceccf02bb
Douglas <douglasobara97@gmail.com>
2022-12-11 23:13:09 +0300

    Refactor  & add routes

--- src/components/index.js
+++ src/components/index.js
@@ -2,3 +2,4 @@ export { default as Hero } from "./Hero";
 export { default as StandoutService } from "./StandoutService";
 export { default as EXperience } from "./Experience";
 export { default as Footer } from "./Footer";
+export { default as TopNavbar } from "./TopNavbar";
